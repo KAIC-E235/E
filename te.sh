0c3ee65a261f4c482b942d99f026cfa965fa8cdf
Soni Hikari <58514579+01soni247@users.noreply.github.com>
2023-07-29 20:54:34 +0700

    Update te.sh

--- te.sh
+++ te.sh
@@ -8,7 +8,7 @@ export ARCH=arm64
 rm -rf AnyKernel
 export KBUILD_BUILD_HOST=Axioo
 export KBUILD_BUILD_USER="KazuHikari"
-git clone https://gitlab.com/LeCmnGend/proton-clang -b Clang-15  clang
+git clone https://gitlab.com/LeCmnGend/proton-clang -b clang-15  clang
 
 [ -d "out" ] && rm -rf out || mkdir -p out
 
@@ -36,7 +36,7 @@ function zupload()
 git clone --depth=1 https://github.com/JR205-5000/AnyKernel3-1 -b rosemary AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 MyGO!!!!!-V3Kernel-Lancelot-E235-SUOC.zip *
+zip -r9 MyGO!!!!!-For Roselia SU.zip*
 curl -sL https://git.io/file-transfer | sh
 ./transfer wet Succubus-Kernel-Lamcot-JR205-1.zip
 }
