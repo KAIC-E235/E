d6cd13eb85f8c00af64f643f2ec000b0617f8187
Soni Hikari <58514579+01soni247@users.noreply.github.com>
2023-07-30 20:49:47 +0700

    Update te.sh

--- te.sh
+++ te.sh
@@ -15,7 +15,7 @@ git clone https://gitlab.com/LeCmnGend/proton-clang -b clang-15  clang
 make O=out ARCH=arm64 rosemary_defconfig
 
 PATH="${PWD}/clang/bin:${PATH}" \
-make -j2                O=out \
+make -j8                O=out \
                         ARCH=$ARCH \
                         CC="clang" \
                         CLANG_TRIPLE=aarch64-linux-gnu- \
@@ -36,7 +36,7 @@ function zupload()
 git clone --depth=1 https://github.com/JR205-5000/AnyKernel3-1 -b rosemary AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 MyGO!!!!!-For Roselia SU.zip*
+zip -r9 MyGO-TY8567-Roselia-OC.zip *
 curl -sL https://git.io/file-transfer | sh
 ./transfer wet Succubus-Kernel-Lamcot-JR205-1.zip
 }
